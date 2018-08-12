d133ed2540446b88a6dc1a6231618e3b2a281aae
ruskolo <36356520+ruskolo@users.noreply.github.com>
2018-08-12 12:47:39 +0200

    Update pos_handler2.py

--- pos_handler2.py
+++ pos_handler2.py
@@ -7,7 +7,7 @@ from random import randint
 from binance.client import Client
 from binance.enums import *
 
-client = Client('r4wwOjKaAGLqP12ETukXGDl3197rgAAjTs2c8fMtsWNpH8pEgTeJLmqArlcdQYjc', '6rsJJ5KEPA8crdlgg0MMCZUcoRMOAE998rMuHcaof085bJuSUAoHBwbqTJcQ7LFf')
+client = Client(<API>, <API_SECRET>)
 
 change_since_high_outset = -1.5
 change_since_high_increment = -0.5
